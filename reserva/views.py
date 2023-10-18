542d8217628a5abd7007c3d265adf02249e48959
Wesley <wesleykauan1103@gmail.com>
2023-10-18 14:13:13 -0300

    atualizando

--- reserva/views.py
+++ reserva/views.py
@@ -3,6 +3,7 @@ from .models import Reserva, Stand
 from .form import ReservaForm
 from django.views.generic import ListView,CreateView,DeleteView,DetailView, UpdateView,TemplateView
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.contrib.messages import views
 
@@ -18,32 +19,32 @@ class Index(generic.TemplateView):
 class Add(generic.TemplateView):
     template_name = "reserva/add.html"
     
-class Detalhar(DetailView):
+class Detalhar(LoginRequiredMixin, generic.DetailView):
     model = Reserva
     template_name = "reserva/detalhar.html"  
     context_object_name = 'reserva'
 
-class Listar(ListView):
+class Listar(generic.ListView):
     template_name = "reserva/reservas.html"
     model = Reserva
     context_object_name = 'reserva'
     paginate_by = 3
 
 
-class Cadastrar(views.SuccessMessageMixin, generic.CreateView):
+class Cadastrar(LoginRequiredMixin,views.SuccessMessageMixin, generic.CreateView):
   model = Reserva
   form_class = ReservaForm
   template_name = "reserva/forms.html"
   success_url = reverse_lazy("listar")
   success_message = "Reserva cadastrada com sucesso!"
   
-class Deletar(views.SuccessMessageMixin, generic.DeleteView):
+class Deletar(LoginRequiredMixin,views.SuccessMessageMixin, generic.DeleteView):
   model = Reserva
   template_name = "reserva/deletar.html"
   success_url = reverse_lazy("listar")
   success_message = "Reserva removida com sucesso!"
   
-class Editar(views.SuccessMessageMixin, generic.UpdateView):
+class Editar(LoginRequiredMixin,views.SuccessMessageMixin, generic.UpdateView):
   model = Reserva
   form_class = ReservaForm
   template_name = "reserva/forms.html"
